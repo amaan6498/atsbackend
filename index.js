47f82ac54f075fd9e11d23868052705ef944c5c7
amaan6498 <amaanamn585@gmail.com>
2025-10-11 12:24:04 +0530

    Add start script to package.json and make server port dynamic using environment variables.

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ dotenv.config();
 import { GoogleGenAI } from "@google/genai";
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 app.use(express.json());
 const ai = new GoogleGenAI({
   apiKey: process.env.API_KEY,
